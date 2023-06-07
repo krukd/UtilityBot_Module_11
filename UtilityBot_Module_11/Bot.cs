9d8dc70d7038245a172f765e763cab0cd23183a2
krukd <krukd87@gmail.com>
2023-06-07 21:47:19 +0200

    Добавьте файлы проекта.

---
+++ UtilityBot_Module_11/Bot.cs
@@ -0,0 +1,73 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+//using Telegram.Bot.Extensions.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot.Polling;
+
+namespace UtilityBot_Module_11
+{
+    class Bot : BackgroundService
+    {
+        /// <summary>
+        /// объект, отвеающий за отправку сообщений клиенту
+        /// </summary>
+        private ITelegramBotClient _telegramClient;
+
+        public Bot(ITelegramBotClient telegramClient)
+        {
+            _telegramClient = telegramClient;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _telegramClient.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                new ReceiverOptions() { AllowedUpdates = { } }, // receive all update types
+                cancellationToken: stoppingToken);
+
+            Console.WriteLine("Bot started");
+        }
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            //  Обрабатываем нажатия на кнопки  из Telegram Bot API: https://core.telegram.org/bots/api#callbackquery
+            if (update.Type == UpdateType.CallbackQuery)
+            {
+                await _telegramClient.SendTextMessageAsync(update.CallbackQuery.From.Id, $"Данный тип сообщений не поддерживается. Пожалуйста отправьте текст.", cancellationToken: cancellationToken);
+                return;
+            }
+
+            // Обрабатываем входящие сообщения из Telegram Bot API: https://core.telegram.org/bots/api#message
+            if (update.Type == UpdateType.Message)
+            {
+                switch (update.Message!.Type)
+                {
+                    case MessageType.Text:
+                        await _telegramClient.SendTextMessageAsync(update.Message.From.Id, $"Длина сообщения: {update.Message.Text.Length} знаков", cancellationToken: cancellationToken);
+                        return;
+                    default: // unsupported message
+                        await _telegramClient.SendTextMessageAsync(update.Message.From.Id, $"Данный тип сообщений не поддерживается. Пожалуйста отправьте текст.", cancellationToken: cancellationToken);
+                        return;
+                }
+            }
+        }
+
+        Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(errorMessage);
+            Console.WriteLine("Waiting 10 seconds before retry");
+            Thread.Sleep(10000);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ UtilityBot_Module_11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telegram.Bot;
+
+
+namespace UtilityBot_Module_11
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()
+                .Build();
+
+            Console.WriteLine("Starting Service");
+            await host.RunAsync();
+            Console.WriteLine("Service stopped");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Регистрируем объект TelegramBotClient c токеном подключения
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("6297350690:AAHLT9e0I_kkMW4IHQ6NVJLlDn8JRKOoiCU"));
+            // Регистрируем постоянно активный сервис бота
+            services.AddHostedService<Bot>();
+        }
+    }
+}
